a500985f15756d06b1a73b750057310ea313ae40
Wizock <Rohaanerodasahmed@gmail.com>
2021-10-02 04:31:52 +1000

    triangle shape assembler

--- src/init.h
+++ src/init.h
@@ -1,10 +1,15 @@
 #pragma once
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <cstdlib>
 
 
 bool initGLFW();
 bool initGLEW();
 int initaliseLibrary();
+static void ClearError();
+static bool CheckError(std::string function, std::string file, int line);
