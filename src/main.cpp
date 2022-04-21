b6272cbf933e176bcff20bf4a2009c0d359b890b
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-21 21:06:31 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "window.h"
+
 #include "shaders.h"
+
 #include "triangle.h"
+
 #include "shaderImports.h"
 
 
@@ -8,26 +11,27 @@ std::string VertexShader = ImportShader("src/shaders/VertexShader.shader");
 
 std::string FragmentShader = ImportShader("src/shaders/FragmentShader.shader");
 
+int main() {
+    Window mainWindow;
+    mainWindow.WindowInstance(600, 800, "hello world");
+    //glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+    unsigned int shader = CreateShader(VertexShader, FragmentShader);
 
-int main()
-{
-	Window mainWindow;
-	mainWindow.WindowInstance(600, 800, "hello world");
-	//glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
-	unsigned int shader = CreateShader(VertexShader, FragmentShader);
+    float positions[] = {
+        -0.5 f,
+        -0.5 f,
+        0.0 f,
+        0.5 f,
+        -0.5 f,
+        0.0 f,
+        0.0 f,
+        0.5 f,
+        0.0 f
+    };
+    while (!mainWindow.closed()) {
+        mainWindow.clear();
+        DrawTriangle(positions, 3, shader);
+        mainWindow.update();
 
-	float positions[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
-	while (!mainWindow.closed())
-	{
-		mainWindow.clear();
-		DrawTriangle(positions, 3, shader);
-		mainWindow.update();
-		
-		
-		
-	}
+    }
 }
