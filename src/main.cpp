ae97890e4b6a071fe388e0bdf48996289cf51bae
Wizock <silent.death3500@gmail.com>
2022-05-18 17:25:38 +1000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,19 @@ std::string FragmentShader = ImportShader("src/shaders/FragmentShader.shader");
 
 int main() {
     Window mainWindow;
+
     mainWindow.WindowInstance(600, 800, "hello world");
-    //glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
     unsigned int shader = CreateShader(VertexShader, FragmentShader);
 
     float positions[] = {
-        -0.5f,
-        -0.5f,
-        0.0f,
-        0.5f,
-        -0.5f,
-        0.0f,
-        0.0f,
-        0.5f,
-        0.0f
+        -0.5f,-0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.0f,0.5f,0.0f
     };
+
     while (!mainWindow.closed()) {
+
         mainWindow.clear();
         DrawTriangle(positions, 3, shader);
         mainWindow.update();
